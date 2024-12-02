727f40d6d44e4434ca02ec93d8b7f402b3a3c567
Sylvie <sylvie@tycrek.com>
2024-12-01 22:26:20 -0700

    feat: add path and imports

--- src/Pagery.ts
+++ src/Pagery.ts
@@ -1,1 +1,10 @@
 import pkg from '../deno.json' with { type: 'json' };
+
+import * as path from '@std/path';
+
+import postcss from 'postcss';
+import pug from 'pug';
+
+import Log from './Log.ts';
+import { PageryError } from './PageryError.ts';
+import type { ConfigFile, Options } from './Options.ts';
