f8669e69318e8b1f47b4e4ca60c586af26dd1f37
Sylvie <sylvie@tycrek.com>
2024-12-02 18:26:31 -0700

    fix: .js to .ts

--- test/tailwind.config.ts
+++ test/tailwind.config.ts
@@ -1,8 +1,15 @@
-module.exports = {
+import { Config } from 'tailwindcss';
+
+export default {
 	separator: '_',
+	darkMode: 'class',
+	plugins: [],
 	content: ['./views/**/*.pug'],
 	theme: {
 		extend: {
+			fontFamily: {
+				main: ['"Lato"', 'ui-sans-serif', 'system-ui', 'sans-serif']
+			}
 		}
 	}
-};
+} satisfies Config;
