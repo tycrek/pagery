f7c761e8ceae0f464e5685bc798d573405b403a5
Sylvie <sylvie@tycrek.com>
2024-12-17 01:23:43 -0700

    feat: now works properly as a module!

--- src/Pagery.ts
+++ src/Pagery.ts
@@ -1,37 +1,84 @@
 import { parseArgs } from '@std/cli/parse-args';
-
 import { DEFAULT_OPTIONS, DEFAULT_OPTIONS_RECORD } from './Options.ts';
-import { generate, generateCss as genCss, generatePug as genPug } from './generate.ts';
+import { generate, generateCss, generatePug } from './generate.ts';
 import { arrayify, chdir, errorPrint, join, log } from './utils.ts';
 import type { Options } from './Options.ts';
 
-const processArgs = (opts: Options): Options => {
-	// Change directory if needed
-	if (opts.dir) chdir(opts.dir);
+/**
+ * Pagery generators
+ */
+export default class Pagery {
+	readonly options: Options;
+	private isModule = !import.meta.main;
+	private originalDir = Deno.cwd();
 
-	// Use absolute paths
-	opts.views = join(opts.views);
-	opts.output = join(opts.output);
-
-	// Types that should be an array
-	const arrayFields: ('tailwindFile' | 'postcssPlugins' | 'data' | 'exclude' | 'only')[] = [
-		'tailwindFile',
-		'postcssPlugins',
-		'data',
-		'exclude',
-		'only',
-	];
-
-	// Parse array types
-	for (const field of arrayFields) {
-		if (opts[field] == '' || opts[field] == null) continue;
-		if (typeof opts[field] === 'string') {
-			opts[field] = arrayify(opts[field].split(','));
-		}
+	constructor(options: Options) {
+		this.options = this.processArgs(options);
 	}
 
-	return opts;
-};
+	private processArgs = (opts: Options): Options => {
+		// Change directory if needed
+		if (opts.dir) chdir(opts.dir);
+
+		// Use absolute paths
+		opts.views = join(opts.views);
+		opts.output = join(opts.output);
+
+		// Types that should be an array
+		const arrayFields: ('tailwindFile' | 'postcssPlugins' | 'data' | 'exclude' | 'only')[] = [
+			'tailwindFile',
+			'postcssPlugins',
+			'data',
+			'exclude',
+			'only',
+		];
+
+		// Parse array types
+		for (const field of arrayFields) {
+			if (opts[field] == '' || opts[field] == null) continue;
+			if (typeof opts[field] === 'string') {
+				opts[field] = arrayify(opts[field].split(','));
+			}
+		}
+
+		return opts;
+	};
+
+	/**
+	 * Default generator: produces both Pug and CSS.
+	 * @returns Promise resolving compiled Pug and CSS
+	 */
+	async generate(): Promise<{ pug: { [key: string]: string }; css: { [key: string]: string } }> {
+		const results = await generate(this.options, this.isModule);
+		Deno.chdir(this.originalDir);
+		return Promise.resolve(results);
+	}
+
+	/**
+	 * CSS generator
+	 * @returns Promise resolving compiled CSS
+	 */
+	async css(): Promise<{ [key: string]: string }> {
+		const results = await generateCss(this.options);
+		Deno.chdir(this.originalDir);
+		return Promise.resolve(results);
+	}
+
+	/**
+	 * Pug generator
+	 * @param cssData - CSS data to pass to Pug renderer
+	 * @param userData - JSON data to pass to Pug renderer
+	 * @returns Promise resolving compiled Pug
+	 */
+	async pug(
+		cssData: { [key: string]: string },
+		userData: { [key: string]: JSON } = {},
+	): Promise<{ [key: string]: string }> {
+		const results = await generatePug(this.options, userData, cssData);
+		Deno.chdir(this.originalDir);
+		return Promise.resolve(results);
+	}
+}
 
 // * Check if running via CLI
 if (import.meta.main) {
@@ -48,43 +95,12 @@ if (import.meta.main) {
 		};
 	};
 
-	generate(processArgs(args.config ? processFileDefaults(join(args.config)) : args))
-		.then(() => log.success('Complete'))
-		.catch(errorPrint);
+	try {
+		await new Pagery(args.config ? processFileDefaults(join(args.config)) : args).generate();
+		log.success('Complete');
+	} catch (err) {
+		errorPrint(err);
+	}
 }
-
-/**
- * Default generator: produces both Pug and CSS.
- * @param options - Pagery options
- * @returns Promise resolving compiled Pug and CSS
- */
-export default (options: Options): Promise<{
-	pug: { [key: string]: string };
-	css: { [key: string]: string };
-}> => generate(processArgs(options));
-
-/**
- * CSS generator
- * @param options - Pagery options
- * @returns Promise resolving compiled CSS
- */
-export const generateCss = (options: Options): Promise<
-	{ [key: string]: string }
-> => genCss(processArgs(options));
-
-/**
- * Pug generator
- * @param options - Pagery options
- * @param userData - JSON data to pass to Pug renderer
- * @param cssData - CSS data to pass to Pug renderer
- * @returns Promise resolving compilged Pug
- */
-export const generatePug = (
-	options: Options,
-	userData: { [key: string]: JSON },
-	cssData: { [key: string]: string },
-): Promise<
-	{ [key: string]: string }
-> => genPug(processArgs(options), userData, cssData);
 
 export type { Options };
